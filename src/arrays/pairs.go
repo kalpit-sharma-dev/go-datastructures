629ea56429a6e87002c9356a0ba293495a8170b7
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2021-07-06 16:01:36 -0400

    Adding Pairs BruteForce Solution

--- src/arrays/pairs.go
+++ src/arrays/pairs.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func Pair() {
 	fmt.Println("PAIR")
-	s := 4
+	s := 11
 	arr := []int{7, 2, 10, 9, 3, -6, 1}
 	fmt.Println(arr)
 	// Brute Force Complexity O(n^2)
-	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; j < len(arr)-1; j++ {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
 			if arr[i]+arr[j] == s {
 				fmt.Println(arr[j], " and ", arr[i])
 			}
