2f479d42628efa870e3e9143a708072f50e03210
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2021-07-08 14:44:05 -0400

    Adding changes for triplet Code

--- src/arrays/triplet.go
+++ src/arrays/triplet.go
@@ -6,17 +6,18 @@ func Triplet() {
 
 	fmt.Println("Triplet")
 	//For this solution Array Must be Sorted
-	s := 15
-	arr := []int{1, 2, 3, 5, 7, 9, 10}
+	targetSum := 15
+	arr := []int{-4, -1, 1, 2, 3, 5, 7, 9, 10, 20}
 	for i := 0; i < len(arr); i++ {
 		j := i + 1
 		k := len(arr) - 1
-		find := s - arr[i]
+
 		for j < k {
-			if arr[j]+arr[k] == find {
+			currSum := arr[i] + arr[j] + arr[k]
+			if currSum == targetSum {
 				fmt.Println(arr[i], arr[j], arr[k])
 				break
-			} else if find > arr[j]+arr[k] {
+			} else if targetSum > currSum {
 				j++
 			} else {
 				k--
